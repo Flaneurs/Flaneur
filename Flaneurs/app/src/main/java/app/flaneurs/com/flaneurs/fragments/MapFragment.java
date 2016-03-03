2574b5478f4532f6f763fda6e2da8c62d863b0cc
Mike Price <mprice@zynga.com>
2016-03-02 21:07:36 -0800

    Added map

--- Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/fragments/MapFragment.java
+++ Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/fragments/MapFragment.java
@@ -1,27 +1,21 @@
 package app.flaneurs.com.flaneurs.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import app.flaneurs.com.flaneurs.R;
-import butterknife.ButterKnife;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends SupportMapFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
 
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
-        ButterKnife.bind(this, v);
-
-        return v;
+            }
+        });
     }
 }
