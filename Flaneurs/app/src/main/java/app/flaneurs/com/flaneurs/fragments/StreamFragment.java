f505b19c1807edf328ca4c328b2b95e2b556b378
Kamran Pirwani <kamranp619@gmail.com>
2016-03-05 20:49:19 -0800

    Ensure we are able to reuse our StreamFragment to display all posts as well as specific user posts. Additionally, modify our ProfileActivity to allow surfacing the current user as well as other users profile data. Add a mechanism to view other profiles by clicking on the stream.

--- Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/fragments/StreamFragment.java
+++ Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/fragments/StreamFragment.java
@@ -11,16 +11,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import app.flaneurs.com.flaneurs.R;
+import app.flaneurs.com.flaneurs.activities.ProfileActivity;
+import app.flaneurs.com.flaneurs.models.User;
 import app.flaneurs.com.flaneurs.utils.DividerItemDecoration;
 import app.flaneurs.com.flaneurs.activities.FlanDetailActivity;
 import app.flaneurs.com.flaneurs.adapters.FlanArrayAdapter;
@@ -29,7 +34,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 
-public class StreamFragment extends Fragment implements FlanArrayAdapter.IFlanInteractionListener{
+public class StreamFragment extends Fragment implements FlanArrayAdapter.IFlanInteractionListener {
 
     @Bind(R.id.rvFlans)
     RecyclerView rvFlans;
@@ -39,6 +44,47 @@ public class StreamFragment extends Fragment implements FlanArrayAdapter.IFlanIn
 
     @Bind(R.id.pbStreamLoading)
     ProgressBar pbStreamLoading;
+
+    public static String STREAM_CONFIGURATION_KEY = "STREAM_CONFIGURATION_KEY";
+
+    private StreamConfiguration mStreamConfiguration;
+
+    public StreamConfiguration getStreamConfiguration() {
+        return mStreamConfiguration;
+    }
+
+    public static class StreamConfiguration implements Serializable {
+        private StreamType mStreamType;
+        private ParseUser mUser;
+
+        public StreamType getStreamType() {
+            return mStreamType;
+        }
+
+        public ParseUser getUser() {
+            return mUser;
+        }
+
+        public void setStreamType(StreamType mStreamType) {
+            this.mStreamType = mStreamType;
+        }
+
+        public void setUser(ParseUser mUser) {
+            this.mUser = mUser;
+        }
+    }
+
+    public enum StreamType {
+        AllPosts, User
+    }
+
+    public static StreamFragment createInstance(StreamConfiguration configuration) {
+        StreamFragment streamFragment = new StreamFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(STREAM_CONFIGURATION_KEY, configuration);
+        streamFragment.setArguments(bundle);
+        return streamFragment;
+    }
 
     protected ArrayList<Post> mFlans;
     protected FlanArrayAdapter adapter;
@@ -53,18 +99,44 @@ public class StreamFragment extends Fragment implements FlanArrayAdapter.IFlanIn
 
     private void grabDataFromParse(boolean hideProgressBar) {
         showLoadingDataState(hideProgressBar);
+        StreamConfiguration streamConfiguration = getStreamConfiguration();
+        if (streamConfiguration.getStreamType() == StreamType.AllPosts) {
+            grabAllPostsFromParse();
+        } else if (streamConfiguration.getStreamType() == StreamType.User) {
+            grabAllPostsForUser();
+        }
+    }
+
+    private void grabAllPostsFromParse() {
         ParseQuery<Post> query = ParseQuery.getQuery("Post");
         query.orderByDescending("KEY_POST_DATE");
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> objects, ParseException e) {
-                swipeContainer.setRefreshing(false);
-                mFlans.clear();
-                mFlans.addAll(objects);
-                adapter.notifyDataSetChanged();
-                hideLoadingDataState();
+                onParseResultsReceived(objects);
             }
         });
+    }
+
+    private void grabAllPostsForUser() {
+        ParseUser user = getStreamConfiguration().getUser();
+        ParseQuery<Post> query = ParseQuery.getQuery("Post");
+        query.orderByDescending(Post.KEY_POST_DATE);
+        query.whereEqualTo(Post.KEY_POST_AUTHOR, user);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                onParseResultsReceived(objects);
+            }
+        });
+    }
+
+    private void onParseResultsReceived(List<Post> posts) {
+        swipeContainer.setRefreshing(false);
+        mFlans.clear();
+        mFlans.addAll(posts);
+        adapter.notifyDataSetChanged();
+        hideLoadingDataState();
     }
 
     private void showLoadingDataState(boolean hideProgressBar) {
@@ -97,10 +169,18 @@ public class StreamFragment extends Fragment implements FlanArrayAdapter.IFlanIn
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_stream, container, false);
         ButterKnife.bind(this, v);
+        retrieveStreamConfiguration();
         setupRecyclerView();
         setupPullToRefresh();
         grabDataFromParse(false);
         return v;
+    }
+
+    private void retrieveStreamConfiguration() {
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            mStreamConfiguration = (StreamConfiguration) bundle.getSerializable(STREAM_CONFIGURATION_KEY);
+        }
     }
 
     private void setupPullToRefresh() {
@@ -135,6 +215,19 @@ public class StreamFragment extends Fragment implements FlanArrayAdapter.IFlanIn
     @Override
     public void openDetailView(Post flan) {
         Intent i = new Intent(getActivity(), FlanDetailActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void openProfileView(Post flan) {
+        User user = (User) flan.getAuthor();
+        if (user == null) {
+            Toast.makeText(getActivity(), "The user you are looking for was not a valid User", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent i = new Intent(getActivity(), ProfileActivity.class);
+        i.putExtra(ProfileActivity.PROFILE_TYPE, ProfileActivity.ProfileType.OTHER_USER);
+        i.putExtra(ProfileActivity.USER_ID, user.getObjectId());
         startActivity(i);
     }
 }
