68ac91994890b09833c8db8f5ae23b3310850b25
Mike Price <mprice@zynga.com>
2016-03-05 19:11:30 -0800

    Added simple facebook auth

--- Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/activities/ProfileActivity.java
+++ Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/activities/ProfileActivity.java
@@ -1,15 +1,56 @@
 package app.flaneurs.com.flaneurs.activities;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.bumptech.glide.Glide;
 
 import app.flaneurs.com.flaneurs.R;
+import app.flaneurs.com.flaneurs.adapters.MapStreamPagerAdapter;
+import app.flaneurs.com.flaneurs.fragments.MapFragment;
+import app.flaneurs.com.flaneurs.fragments.StreamFragment;
+import app.flaneurs.com.flaneurs.models.User;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.ivProfileImage)
+    ImageView ivProfileImage;
+
+    @Bind(R.id.tvUsername)
+    TextView tvUsername;
+
+    @Bind(R.id.vpViewPager)
+    ViewPager viewPager;
+
+    @Bind(R.id.psTabs)
+    PagerSlidingTabStrip slidingTabStrip;
+
+    private MapFragment mMapFragment;
+    private StreamFragment mStreamFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+
+        User currentUser = User.currentUser();
+        if (currentUser != null) {
+            Glide.with(this).load(currentUser.getProfileUrl()).into(ivProfileImage);
+            tvUsername.setText(currentUser.getUsername());
+        }
+
+        mMapFragment = new MapFragment();
+        mStreamFragment = new StreamFragment();
+        viewPager.setAdapter(new MapStreamPagerAdapter(getSupportFragmentManager(), mMapFragment, mStreamFragment));
+
+        slidingTabStrip.setViewPager(viewPager);
     }
 }
