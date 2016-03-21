54706f6305cebe956b241b9a77767bbaad71cbbc
Kenneth Pu <kp295@cornell.edu>
2016-03-20 21:48:18 -0700

    add place picker compose screen

--- Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/activities/ComposeActivity.java
+++ Flaneurs/app/src/main/java/app/flaneurs/com/flaneurs/activities/ComposeActivity.java
@@ -1,17 +1,23 @@
 package app.flaneurs.com.flaneurs.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.model.LatLng;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -32,14 +38,17 @@ import butterknife.ButterKnife;
 
 public class ComposeActivity extends AppCompatActivity {
 
+    public static final String TAG = ComposeActivity.class.getSimpleName();
+    public static final int REQUEST_PLACE_PICKER = 37;
+
     @Bind(R.id.ivPicturePreview)
     ImageView ivPicturePreview;
 
     @Bind(R.id.etCaption)
     EditText etCaption;
 
-    @Bind(R.id.tvLocation)
-    TextView tvLocation;
+    @Bind(R.id.btnLocation)
+    Button btnLocation;
 
     @Bind(R.id.postButton)
     Button postButton;
@@ -48,6 +57,8 @@ public class ComposeActivity extends AppCompatActivity {
 
     private double mLat;
     private double mLong;
+
+    private MapFragment mMapFragment;
 
     public final static String COMPOSE_LAT_ID = "COMPOSE_LAT_ID";
     public final static String COMPOSE_LONG_ID = "COMPOSE_LONG_ID";
@@ -67,7 +78,7 @@ public class ComposeActivity extends AppCompatActivity {
         mPicture = BitmapFactory.decodeFile(imageUri);
         ivPicturePreview.setImageBitmap(mPicture);
 
-        tvLocation.setText(Utils.getPrettyAddress(this, mLat, mLong));
+        btnLocation.setText(Utils.getPrettyAddress(this, mLat, mLong));
 
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
@@ -75,7 +86,23 @@ public class ComposeActivity extends AppCompatActivity {
 
         postButton.setPadding(sidePadding, 0, sidePadding, 0);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.flMap, MapFragment.newInstance(false, false, new LatLng(mLat, mLong), null)).commit();
+        mMapFragment = MapFragment.newInstance(false, false, new LatLng(mLat, mLong), null);
+        getSupportFragmentManager().beginTransaction().replace(R.id.flMap, mMapFragment).commit();
+    }
+
+    public void onLocationClicked(View v) {
+        // Construct an intent for the place picker
+        try {
+            PlacePicker.IntentBuilder intentBuilder =
+                    new PlacePicker.IntentBuilder();
+            Intent intent = intentBuilder.build(this);
+            // Start the intent by requesting a result,
+            // identified by a request code.
+            startActivityForResult(intent, REQUEST_PLACE_PICKER);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            Log.d(TAG, "Unable to launch place picker.");
+            Toast.makeText(this, "Error - Unable to launch place picker!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onPostButtonClicked(View v) {
@@ -100,7 +127,7 @@ public class ComposeActivity extends AppCompatActivity {
         ParseFile file = new ParseFile("picture.jpg", bytearray);
         newPost.setImage(file);
 
-        String address = tvLocation.getText().toString();
+        String address = btnLocation.getText().toString();
         newPost.setAddress(address);
 
         newPost.saveInBackground(new SaveCallback() {
@@ -116,5 +143,24 @@ public class ComposeActivity extends AppCompatActivity {
 
         setResult(RESULT_OK, data);
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PLACE_PICKER
+                && resultCode == Activity.RESULT_OK) {
+
+            // The user has selected a place. Extract the name and latLng.
+            final Place place = PlacePicker.getPlace(data, this);
+            final CharSequence name = place.getName();
+            final LatLng location = place.getLatLng();
+            mLat = location.latitude;
+            mLong = location.longitude;
+
+            btnLocation.setText(name);
+            mMapFragment.markLatLng(location);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
